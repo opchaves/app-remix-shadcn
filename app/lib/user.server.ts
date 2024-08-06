e0248eac7d541287c63d1eabb7b6a9e405c7b6c3
Paulo Chaves <opaulochaves@gmail.com>
2024-08-06 19:02:22 -0300

    refactor user.server

--- app/lib/user.server.ts
+++ app/lib/user.server.ts
@@ -1,52 +1,44 @@
 import { AppLoadContext } from "@remix-run/node";
 import bcrypt from "bcryptjs";
-import { SqliteError } from "better-sqlite3";
 import { and, eq } from "drizzle-orm";
 
 import { password, user } from "@/db.server/schema";
+import env from "./env.server";
+
+type CreateParams = {
+	displayName: string;
+	email: string;
+	fullName: string;
+	password: string;
+};
 
 export async function createUser(
 	{ DB }: AppLoadContext,
-	{
-		displayName,
-		email,
-		fullName,
-		password: unhashedPassword,
-	}: {
-		displayName: string;
-		email: string;
-		fullName: string;
-		password: string;
-	},
+	{ displayName, email, fullName, password: unhashedPassword }: CreateParams,
 ) {
 	try {
-		const createdUser = (
-			await DB.insert(user)
-				.values({
-					displayName,
-					email,
-					fullName,
-				})
-				.returning({
-					id: user.id,
-					displayName: user.displayName,
-					email: user.email,
-					fullName: user.fullName,
-				})
-		)[0];
+		const [createdUser] = await DB.insert(user)
+			.values({ displayName, email, fullName })
+			.returning({
+				id: user.id,
+				displayName: user.displayName,
+				email: user.email,
+				fullName: user.fullName,
+			});
+
 		if (!createdUser) return null;
 
 		try {
-			const inserted = await DB.insert(password)
+			const [inserted] = await DB.insert(password)
 				.values({
 					userId: createdUser.id,
-					password: await bcrypt.hash(unhashedPassword, 11),
+					password: await bcrypt.hash(unhashedPassword, env.HASH_SALT),
 				})
-				.returning({
-					userId: password.userId,
-				});
-			if (inserted[0]?.userId !== createdUser.id)
+				.returning({ userId: password.userId });
+
+			if (inserted?.userId !== createdUser.id) {
 				throw new Error("Failed to create user");
+			}
 		} catch (reason) {
 			console.error(reason);
 			try {
@@ -75,70 +67,62 @@ export async function createUser(
 }
 
 export async function getUserById({ DB }: AppLoadContext, userId: string) {
-	return (
-		(await DB.query.user.findFirst({
-			columns: {
-				id: true,
-				displayName: true,
-				email: true,
-				fullName: true,
-			},
-			where: eq(user.id, userId),
-		})) ?? null
-	);
+	const aUser = await DB.query.user.findFirst({
+		columns: { id: true, displayName: true, email: true, fullName: true },
+		where: eq(user.id, userId),
+	});
+	return aUser;
 }
+
+type GetUserParams = {
+	email: string;
+	password: string;
+};
 
 export async function getUserByLogin(
 	{ DB }: AppLoadContext,
-	{ email, password: unhashedPassword }: { email: string; password: string },
+	{ email, password: unhashedPassword }: GetUserParams,
 ) {
 	const foundUser = await DB.query.user.findFirst({
-		columns: {
-			id: true,
-			displayName: true,
-			email: true,
-			fullName: true,
-		},
+		columns: { id: true, displayName: true, email: true, fullName: true },
 		where: eq(user.email, email),
 	});
-	if (!foundUser) return null;
+	if (!foundUser) return;
 
 	const foundPassword = await DB.query.password.findFirst({
-		columns: {
-			password: true,
-		},
+		columns: { password: true },
 		where: and(eq(password.userId, foundUser.id)),
 	});
-	if (!foundPassword) return null;
+	if (!foundPassword) return;
 
 	const passwordMatch = await bcrypt.compare(
 		unhashedPassword,
 		foundPassword.password,
 	);
-	if (!passwordMatch) return null;
+	if (!passwordMatch) return;
 
 	return foundUser;
 }
 
+type UpdateParams = {
+	displayName: string;
+	fullName: string;
+};
+
 export async function updateUser(
 	{ DB }: AppLoadContext,
 	userId: string,
-	{ displayName, fullName }: { displayName: string; fullName: string },
+	{ displayName, fullName }: UpdateParams,
 ) {
-	return (
-		(
-			await DB.update(user)
-				.set({
-					displayName,
-					fullName,
-				})
-				.where(eq(user.id, userId))
-				.returning({
-					id: user.id,
-					displayName: user.displayName,
-					email: user.email,
-					fullName: user.fullName,
-				})
-		)[0] ?? null
-	);
+	const [aUser] = await DB.update(user)
+		.set({ displayName, fullName })
+		.where(eq(user.id, userId))
+		.returning({
+			id: user.id,
+			displayName: user.displayName,
+			email: user.email,
+			fullName: user.fullName,
+		});
+
+	return aUser;
 }
